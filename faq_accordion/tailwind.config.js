f03e1b71942116b540f601280fbb9c734706af26
Anish Singla <singlaanish56@gmail.com>
2024-03-06 03:41:14 +0530

    First Commit for FAQ Accordian

---
+++ faq_accordion/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screen:{
+      sm:'375px',
+      md: '768px',
+      lg: '1440px',
+    },
+    extend: {
+      colors:{
+        White: "hsl(0, 0%, 100%)",
+        Lightpink: "hsl(275, 100%, 97%)",
+        Grayishpurple: "hsl(292, 16%, 49%)",
+        Darkpurple: "hsl(292, 42%, 14%)"        
+      },
+      fontFamily: {
+        libre: ['"Work Sans', ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+}
+
